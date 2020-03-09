100d85cb019cce8578047ac530d67204ef58553b
Roman Grubic <grubic.roman@gmail.com>
2020-03-09 10:18:33 +0000

    Fixed spelling mistake causing env vars not to be pulled!

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+from os import path
+if path.exists("env.py"):
+    import env
 
 if os.environ.get('DEVELOPMENT'):
     development = True
--- eny.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ["DEVELOPMENT"] = 1
